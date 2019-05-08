1a2a715e2198c8c0d3a5eb34bd8c64eb513fbe83
HandiSutriyan <handisutrian@gmail.com>
2019-05-08 14:02:51 +0700

    Updates

---
+++ precache-manifest.fea6afd0df80f66057a82b8173770dd5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "017951ba6a0ffdfd0f6db70ff42e1aaa",
+    "url": "/bank-modul/static/media/pp.017951ba.jpg"
+  },
+  {
+    "revision": "2b0b6903f2e28024de1ded2ed8435063",
+    "url": "/bank-modul/static/media/news.2b0b6903.jpg"
+  },
+  {
+    "revision": "cf66da91da35efcfc59fc9924925d5ea",
+    "url": "/bank-modul/static/media/insb.cf66da91.jpg"
+  },
+  {
+    "revision": "37d5ebc5a15155696f13943e4252deec",
+    "url": "/bank-modul/static/media/beenst.37d5ebc5.png"
+  },
+  {
+    "revision": "23b6a7b242877c34e23d",
+    "url": "/bank-modul/static/js/runtime~main.23b6a7b2.js"
+  },
+  {
+    "revision": "ae8e72d2209eec610a69",
+    "url": "/bank-modul/static/js/main.ae8e72d2.chunk.js"
+  },
+  {
+    "revision": "07fb3356c610bc888e5c",
+    "url": "/bank-modul/static/js/1.07fb3356.chunk.js"
+  },
+  {
+    "revision": "ae8e72d2209eec610a69",
+    "url": "/bank-modul/static/css/main.d8abe925.chunk.css"
+  },
+  {
+    "revision": "06888e1661c67758d4a58d3867c37258",
+    "url": "/bank-modul/index.html"
+  }
+];
